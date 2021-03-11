cd3328c646ceaf175058ca3b45177db485133404
Uricle Knudel <uricle@gmail.com>
2021-03-11 23:18:05 +0900

    ポート番号,パスワード,文字エンコード指定対応

--- ircbot.js
+++ ircbot.js
@@ -8,26 +8,34 @@ const server = process.env.SERVER;
 const nick = process.env.NICK;
 const targetChannel = process.env.TARGET_CHANNEL;
 const channels = [targetChannel];
-
+const password = process.env.PASSWORD;
+const port = process.env.PORT;
 const BearerToken = process.env.BEARER_TOKEN;
+const ServerEncode = process.env.SERVER_ENCODE || "JIS";
+const ClientEncode = process.env.CLIENT_ENCODE || "UTF8";
 const tweetRe = /https?:\/\/(?:mobile\.)?twitter.com\/.+?\/status\/(\d+)/;
+const DEBUG = false;
 
 const client = new Twitter({
     bearer_token: BearerToken
 });
-
+// console.log(`s:${ServerEncode}`);
+// console.log(`c:${ClientEncode}`);
 ircserve();
 
 function ircserve()
 {
-    const client = new irc.Client(server, nick, {channels: channels});
+    const client = new irc.Client(server, nick, {channels: channels, port: port, password: password});
     client.addListener('message', async function (from, to, message) {
-        // var str = jconv.convert(message, 'JIS', 'UTF8');
-        // console.log('from:' + from + ' to:' + to + ' :message' + str);
-        const found = tweetRe.exec(message);
+        var str = (ServerEncode == ClientEncode) ? message : jconv.convert(message, ServerEncode, ClientEncode);
+        if ( DEBUG ) console.log('from:' + from + ' to:' + to + ' :message' + str);
+        const found = tweetRe.exec(str);
         if ( found ) {
             const msgs = await tweetget(found[1]);
-            msgs.forEach( m => client.notice(to, jconv.convert(m, 'UTF8', 'JIS')) );
+            msgs.forEach( m => {
+                const sendmessage = (ServerEncode == ClientEncode) ? m : jconv.convert(m, ClientEncode, ServerEncode);
+                client.notice(to, sendmessage)
+            });
         }
     });
 }
